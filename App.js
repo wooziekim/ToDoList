d9a1332a705f10b9469a108385ec0f3cfdd9fe7e
kimtaewoo <wooziekim@gmail.com>
2020-04-22 15:50:28 +0900

    Coding the UI part 1

--- App.js
+++ App.js
@@ -1,19 +1,64 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  StatusBar, 
+  TextInput, 
+  Dimensions 
 
-export default function App() {
+} from 'react-native';
+
+const {height, width} = Dimensions.get("window");
+
+export default class App extends React.Component {
+  render(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle="light-content" />
+      <Text style={styles.title}>Kawaii To Do</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input} placeholder={"New To Do"} />
+      </View>
     </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F23657',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  title:{
+    color:"white",
+    fontSize: 30,
+    marginTop: 50,
+    marginBottom: 30,
+    fontWeight: "200"
+  },
+  card:{
+    backgroundColor:"white",
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50,50,50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset:{
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3,
+
+      }
+    })    
+    
+  }
 });
